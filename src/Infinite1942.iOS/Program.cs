9107940d5cdebc02482258f8bc90dae0025210ec
Ernesto Rojo <erojo@ernestorojo.com>
2026-01-17 13:39:08 -0600

    Added an iOS version of the project

---
+++ src/Infinite1942.iOS/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Foundation;
+using Microsoft.Xna.Framework;
+using UIKit;
+
+namespace Infinite1942.iOS
+{
+    [Register("AppDelegate")]
+    class Program : UIApplicationDelegate
+    {
+        private static Game game;
+
+        internal static void RunGame()
+        {
+            game = new Infinite1942Game();
+            game.Run();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, typeof(Program));
+        }
+
+        public override void FinishedLaunching(UIApplication app)
+        {
+            RunGame();
+        }
+    }
+}
